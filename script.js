930eb8ac15f38f662eb7510db7e7819b54874f03
JessYev97 <jessmyeverino@gmail.com>
2024-08-20 16:24:30 -0500

    link js file

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+//be kind class = blue (the button) 
+const beKind = document.getElementById("blue").addEventListener("click", alertFunction);
+
+function alertFunction() {
+  alert("did it work?"); 
+}
+
+console.log("Is it linked?"); 
